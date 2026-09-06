7f101aaec927c5f384b541657e7d8e84dbc6e9d9
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-09-06 19:10:20 -0300

    Add/Update src/main/java/com/destructivemod/item/ModItems.java

--- src/main/java/com/destructivemod/item/ModItems.java
+++ src/main/java/com/destructivemod/item/ModItems.java
@@ -1,7 +1,6 @@
 package com.destructivemod.item;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
