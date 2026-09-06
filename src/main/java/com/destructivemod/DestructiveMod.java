6d9b3b6a8ad98feeb73b6f9ae25d09ef06350862
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-09-06 18:24:45 -0300

    Add src/main/java/com/destructivemod/DestructiveMod.java

---
+++ src/main/java/com/destructivemod/DestructiveMod.java
@@ -0,0 +1,18 @@
+package com.destructivemod;
+
+import net.fabricmc.api.ModInitializer;
+import com.destructivemod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DestructiveMod implements ModInitializer {
+    public static final String MOD_ID = "destructivemod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initializing Destructive Mod");
+        ModItems.registerItems();
+        LOGGER.info("Destructive Mod initialized!");
+    }
+}
